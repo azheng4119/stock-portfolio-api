ccfe97649b4f1d443a3c3076f381d79b5ecc3727
azheng4119 <azheng4119@bths.edu>
2019-09-28 01:22:43 -0400

    Setting up express server

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+
+const server = express();
+
+server.use(cors)
+
+
+server.listen(port,() => console.log(`Server listening on port ${port}`));
+
