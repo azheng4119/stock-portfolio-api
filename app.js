6e324410d6efc810913c9f38f4c0cad5387b6b3a
azheng4119 <azheng4119@bths.edu>
2019-09-30 22:16:04 -0400

    Api Url to symbols

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const server = express();
 
 server.use(cors)
 
+server.get('/',(req,res,next) => {
+    console.log(1222);
+    res.send('User path /symbols to search')
+})
 
 server.listen(port,() => console.log(`Server listening on port ${port}`));
 
--- routes/symbols.js
+++ routes/symbols.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const symbols = express();
+const axios = require('axios');
 
-symbols.get('/',(req,res,next) => {
-    
+const apiUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=ew&interval=5min&apikey=UUH5819YQ2VPHIN0`
+
+const getSymbol = async (symbol) => {
+    try{
+        let { data } = axios.get(apiUrl+symbol);
+        if (data[`Error Message`]) return false;
+        else{
+            return Object.values(data["Time Series (Daily)"])[0];
+        }
+    } catch (error){
+        console.log(error);
+    }
+}
+
+symbols.get('/:symbol',(req,res,next) => {
+    let symbol = req.params.symbol;
+    if (req.params.symbol){
+        let symbolData = getSymbol(symbol);
+        res.send(symbolData);
+    }else{
+        res.status(404).send();
+    }
 }
 
 )
