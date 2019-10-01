065c11d7c99f0e84b5fbe36b4947123e8672f228
azheng4119 <azheng4119@bths.edu>
2019-09-30 23:13:55 -0400

    Searching for symbol route

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-const port = process.env.PORT || 5000;
-
+const port = process.env.PORT || 3000;
+const symbols = require('./routes/symbols')
 const server = express();
 
-server.use(cors)
+server.use(cors());
+
+server.use('/symbols',symbols);
 
 server.get('/',(req,res,next) => {
-    console.log(1222);
     res.send('User path /symbols to search')
 })
 
--- routes/symbols.js
+++ routes/symbols.js
@@ -2,12 +2,15 @@ const express = require('express');
 const symbols = express();
 const axios = require('axios');
 
-const apiUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=ew&interval=5min&apikey=UUH5819YQ2VPHIN0`
+// const apiUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=ew&interval=5min&apikey=UUH5819YQ2VPHIN0`
 
 const getSymbol = async (symbol) => {
     try{
-        let { data } = axios.get(apiUrl+symbol);
-        if (data[`Error Message`]) return false;
+        let url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${symbol}&interval=5min&apikey=UUH5819YQ2VPHIN0`
+        console.log(url);
+        let { data } = await axios.get(url);
+        console.log(data);
+        if (data[`Error Message`]) return "No Matching Symbol";
         else{
             return Object.values(data["Time Series (Daily)"])[0];
         }
@@ -16,14 +19,17 @@ const getSymbol = async (symbol) => {
     }
 }
 
-symbols.get('/:symbol',(req,res,next) => {
+symbols.get('/:symbol',async (req,res,next) => {
     let symbol = req.params.symbol;
-    if (req.params.symbol){
-        let symbolData = getSymbol(symbol);
-        res.send(symbolData);
+    if (symbol){
+        let symbolData = await getSymbol(symbol);
+        console.log(symbolData)
+        res.json(symbolData);
     }else{
         res.status(404).send();
     }
 }
 
 )
+
+module.exports = symbols
