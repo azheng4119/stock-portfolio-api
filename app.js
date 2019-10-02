eb5dce75d886d30fe22ccb798f12198cca8ee564
azheng4119 <azheng4119@bths.edu>
2019-10-02 19:37:03 -0400

    symbols fetching

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 3000;
 const symbols = require('./routes/symbols')
-const users = require('./routes/users')
+// const users = require('./routes/users')
 const server = express();
 
 server.use(cors());
 
 server.use('/symbols',symbols);
-server.use('/user',users);
+// server.use('/user',users);
 
 server.get('/',(req,res,next) => {
     res.send('User path /symbols to search')
