2e27557458a4552eab9fbfc0c20fad5a196907bd
azheng4119 <azheng4119@bths.edu>
2019-10-06 00:50:45 -0400

    login function

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,15 @@ const md5 = require('blueimp-md5')
 users.use(bodyParser.json())
 
 
+users.post('/login',async (req,res,next) => {
+    console.log(req.body);
+    await UserModel.findOne({
+         where: {email : req.body.email,password : req.body.password} }).then(project => {
+            if (project !== null) res.status(200).send("Success");
+    })
+    res.status(400).send("Denied");
+})
+
 users.post('/register', async (req,res,next) => {
     const userData = req.body;
     userData.password = md5(userData.password);
